0b55347df0c76ba31c8d4c443f37f2d3b707b187
Kyle Lovell <ky2vell@gmail.com>
2020-06-11 04:07:08 -0400

    stretch: explored recharts library, styled charts on theme switch

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import Chart from "./Chart";
+import React from 'react';
+import Chart from './Chart';
 
 const Charts = ({ coinData }) => {
   return (
-    <div className="charts">
+    <div className='charts'>
       {coinData.map(coin => (
-        <div className="chart__container" key={coin.name}>
-          <h2 className="coin__title">{coin.name}</h2>
-          <h4 className="coin__symbol">{coin.symbol}</h4>
-          <div className="coin__logo">
-            <img src={coin.image} height="40" alt={coin.name} />
+        <div className='chart__container' key={coin.name}>
+          <div className='coin_info_container'>
+            <div className='coin__logo'>
+              <img src={coin.image} height='40' alt={coin.name} />
+            </div>
+            <h2 className='coin__title'>{coin.name}</h2>
+            <h5 className='coin__symbol'>{coin.symbol}</h5>
           </div>
           <Chart sparklineData={coin.sparkline_in_7d.price} />
         </div>
